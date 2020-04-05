a962fe44c9bf898364cbce9d79357682be486209
George McCadden III <thethirdone.g@gmail.com>
2020-04-04 21:54:05 -0700

    Yall  cannot see my passwords

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Importing the package that I need in this file.
 
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 
 // Now I can create routes in a simpler way.
 
 const app = express();
-const mongose = require('mongoose');
 
 
 // Middlewares
@@ -28,7 +29,12 @@ app.get('/posts', (req, res) => {
 
 
 // Connecting to Database
-mongoose.connect('')
+mongoose.connect(process.env.DATABASE_CONNECTION,
+    { useNewUrlParser: true, 
+        useUnifiedTopology: true }, 
+    () => {
+    console.log('connected to Database!');
+});
 
 // I've gotta start listening to the server.
 
