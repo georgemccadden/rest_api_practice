fdb872d76861545dba76ee6c0f1bc3ac20904a68
George McCadden III <thethirdone.g@gmail.com>
2020-04-04 20:55:13 -0700

    Just want to get things up and running

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express(); // Now I can create routes in a simpler way.
 
 // Routes
 
+app.get('/', (req, res) => {
+    res.send('We are running');
+});
+
 app.listen(3000); // I've gotta start listening to the server.
 
